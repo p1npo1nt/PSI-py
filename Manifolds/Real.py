0a872940a5b25a8395cde26adc9644a30736ac7a
p1npo1nt <48339921+p1npo1nt@users.noreply.github.com>
2023-12-28 10:01:44 -0500

    Create Real.py

---
+++ Manifolds/Real.py
@@ -0,0 +1,8 @@
+from sympy import *
+from sympy.geometry import *
+from sympy.diffgeom import Manifold
+from geomstats import *
+import geomstats.datasets.utils as data_utils
+from matplotlib import pyplot as plt
+
+
